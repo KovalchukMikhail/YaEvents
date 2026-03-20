7d0d5a8edd2855c19c18a9205e1b2a4e8268069f
KovalchukMikhail <kovalchuckmikhail@yandex.ru>
2026-03-20 14:31:52 +0300

    в инъектор зависемостей добавлен репозиторий

---
+++ Infrastructure/DependencyInjection.cs
@@ -0,0 +1,19 @@
+﻿using System.Runtime.CompilerServices;
+using YaEvents.Application.Services.EventService;
+using YaEvents.Application.Services.Interfaces;
+using YaEvents.Data.Models;
+using YaEvents.Infrastructure.Repositories.EventsRepository;
+using YaEvents.Infrastructure.Repositories.Interfaces;
+
+namespace YaEvents.Infrastructure
+{
+    public static class DependencyInjection
+    {
+        public static IServiceCollection AddInfrastructure(this IServiceCollection services)
+        {
+            services.AddScoped<IRepository<Event>, EventsRepository>();
+
+            return services;
+        }
+    }
+}
