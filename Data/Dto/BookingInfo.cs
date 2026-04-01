f640b3ad2378179c23b041b0ecc5c20afd803261
KovalchukMikhail <kovalchuckmikhail@yandex.ru>
2026-04-01 11:00:03 +0300

    Созданы модели для бронирования

---
+++ Data/Dto/BookingInfo.cs
@@ -0,0 +1,6 @@
+﻿using YaEvents.Infrastructure.Enums;
+
+namespace YaEvents.Data.Dto
+{
+    public record BookingInfo(Guid Id, Guid EventId, BookingStatus Status, DateTime CreatedAt, DateTime? ProcessedAt);
+}
