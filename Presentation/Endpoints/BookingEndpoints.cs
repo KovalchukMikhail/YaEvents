2f4dc6b6d8cd0d7d1b73547d7e7d32fe4dc58795
KovalchukMikhail <kovalchuckmikhail@yandex.ru>
2026-04-01 11:05:50 +0300

    Добавлены эндпоинты для событий и бронирований

---
+++ Presentation/Endpoints/BookingEndpoints.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using YaEvents.Application.Services.Interfaces;
+using YaEvents.Infrastructure.Exceptions;
+
+namespace YaEvents.Presentation.Endpoints
+{
+    public class BookingEndpoints
+    {
+        public static async Task<IResult> GetBooking(Guid id, IBookingService bookingService, CancellationToken token = default)
+        {
+            var bookingDto = await bookingService.GetBookingByIdAsync(id, token: token);
+            if (bookingDto != null)
+                return Results.Ok(bookingDto);
+            else
+                throw new NotFoundException("Не удалось получить объект бронирования") { EntityId = id };
+        }
+    }
+}
---
+++ Presentation/Endpoints/EventEndpoints.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Http.HttpResults;
+using Microsoft.AspNetCore.Mvc;
+using YaEvents.Application.Services.Interfaces;
+using YaEvents.Data.Dto;
+using YaEvents.Infrastructure.Enums;
+using YaEvents.Infrastructure.Exceptions;
+
+namespace YaEvents.Presentation.Endpoints
+{
+    public class EventEndpoints
+    {
+        public static async Task<IResult> PostBooking(Guid id, IEventService eventService, IBookingService bookingService, HttpContext context, CancellationToken token = default)
+        {
+            var eventDto = await eventService.GetEvent(id, token: token);
+            if (eventDto != null)
+            {
+                if (eventDto.Status != EventStatus.Removed)
+                {
+                    var bookingInfo = await bookingService.CreateBookingAsync(eventDto.Id, token: token);
+                    var url = $"{context.Request.Scheme}://{context.Request.Host}/bookings/{bookingInfo.Id}";
+
+                    return Results.Accepted(url, bookingInfo);
+                }
+                else
+                    throw new ValidationException("Не удалось создать объект бронирования так как объект события помечен как удаленный") { EntityId = id };
+            }
+            else
+                throw new NotFoundException("Не удалось создать объект бронирования так как объект события с указанным Id отсутствует") { EntityId = id };
+        }
+    }
+}
