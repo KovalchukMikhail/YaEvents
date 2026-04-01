f09185b8441abe53bcc1f8ebd463577cec023693
KovalchukMikhail <kovalchuckmikhail@yandex.ru>
2026-04-01 11:04:49 +0300

    Добавлен фоновый сервис обработки бронирований

---
+++ Application/BackgroundServices/BookingsBackgroundService.cs
@@ -0,0 +1,46 @@
+﻿using System.Runtime.CompilerServices;
+using YaEvents.Application.Services.Interfaces;
+using YaEvents.Data.Models;
+using YaEvents.Infrastructure.Repositories.EventsRepository;
+using YaEvents.Infrastructure.Repositories.Interfaces;
+
+namespace YaEvents.Application.BackgroundServices
+{
+    public class BookingsBackgroundService : BackgroundService
+    {
+        private readonly ILogger<BookingsBackgroundService> _logger;
+        private readonly IServiceScopeFactory _scopeFactory;
+
+        public BookingsBackgroundService(ILogger<BookingsBackgroundService> logger, IServiceScopeFactory scopeFactory)
+        {
+            _logger = logger;
+            _scopeFactory = scopeFactory;
+        }
+
+        protected override async Task ExecuteAsync(CancellationToken token)
+        {
+            _logger.LogInformation("BookingsBackgroundService запущен");
+
+            while (!token.IsCancellationRequested)
+            {
+                try
+                {
+                    var scope = _scopeFactory.CreateScope();
+                    var bookingService = scope.ServiceProvider.GetRequiredService<IBookingService>();
+                    await bookingService.ProcessBookings(token);
+                }
+                catch(OperationCanceledException) when (token.IsCancellationRequested)
+                {
+                    break;
+                }
+                catch (Exception ex)
+                {
+                    _logger.LogError(ex, "Ошибка при обработке бронирования");
+                }
+
+            }
+
+            _logger.LogInformation("BookingsBackgroundService остановлен");
+        }
+    }
+}
--- Application/DependencyInjection.cs
+++ Application/DependencyInjection.cs
@@ -1,4 +1,6 @@
-﻿using YaEvents.Application.Services.EventService;
+﻿using YaEvents.Application.BackgroundServices;
+using YaEvents.Application.Services.BookingService;
+using YaEvents.Application.Services.EventService;
 using YaEvents.Application.Services.Interfaces;
 
 namespace YaEvents.Application
@@ -8,6 +10,9 @@ namespace YaEvents.Application
         public static IServiceCollection AddApplication(this IServiceCollection services)
         {
             services.AddScoped<IEventService, EventService>();
+            services.AddScoped<IBookingService, BookingService>();
+
+            services.AddHostedService<BookingsBackgroundService>();
 
             return services;
         }
