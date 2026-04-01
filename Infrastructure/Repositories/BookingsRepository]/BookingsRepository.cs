d1c15da0a13b4dfc4c122f5c751f54fb802df0c8
KovalchukMikhail <kovalchuckmikhail@yandex.ru>
2026-04-01 11:02:20 +0300

    Создан репозитория для объектов бронирований

---
+++ Infrastructure/Repositories/BookingsRepository]/BookingsRepository.cs
@@ -0,0 +1,36 @@
+﻿using YaEvents.Data.Models;
+using YaEvents.Infrastructure.Repositories.Interfaces;
+
+namespace YaEvents.Infrastructure.Repositories.EventsRepository
+{
+    public class BookingsRepository : IRepository<Booking>
+    {
+        protected static Dictionary<Guid, Booking> _bookings = new Dictionary<Guid, Booking>();
+        public async Task<Booking> Add(Booking booking, CancellationToken token = default)
+        {
+            _bookings[booking.Id] = booking;
+
+            return booking;
+        }
+
+        public async Task Change(Booking booking, CancellationToken token = default)
+        {
+            _bookings[booking.Id] = booking;
+        }
+
+        public async Task<bool> Delete(Guid id, CancellationToken token = default)
+        {
+            return _bookings.Remove(id);
+        }
+
+        public async Task<Booking?> Get(Guid id, CancellationToken token = default)
+        {
+            return _bookings.GetValueOrDefault(id);
+        }
+
+        public async Task<IEnumerable<Booking>> GetAll(CancellationToken token = default)
+        {
+            return _bookings.Values;
+        }
+    }
+}
