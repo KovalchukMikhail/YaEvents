657f9b1ebe3452223eb94cbd37e43abf697be5b0
KovalchukMikhail <kovalchuckmikhail@yandex.ru>
2026-04-02 11:01:20 +0300

    Из метода PostBooking вынесена логика валидации события

--- Presentation/Endpoints/EventEndpoints.cs
+++ Presentation/Endpoints/EventEndpoints.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.AspNetCore.Http.HttpResults;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Diagnostics;
 using YaEvents.Application.Services.Interfaces;
 using YaEvents.Data.Dto;
 using YaEvents.Infrastructure.Enums;
@@ -11,21 +12,10 @@ namespace YaEvents.Presentation.Endpoints
     {
         public static async Task<IResult> PostBooking(Guid id, IEventService eventService, IBookingService bookingService, HttpContext context, CancellationToken token = default)
         {
-            var eventDto = await eventService.GetEvent(id, token: token);
-            if (eventDto != null)
-            {
-                if (eventDto.Status != EventStatus.Removed)
-                {
-                    var bookingInfo = await bookingService.CreateBookingAsync(eventDto.Id, token: token);
-                    var url = $"{context.Request.Scheme}://{context.Request.Host}/bookings/{bookingInfo.Id}";
+            var bookingInfo = await bookingService.CreateBookingAsync(id, token: token);
+            var url = $"{context.Request.Scheme}://{context.Request.Host}/bookings/{bookingInfo.Id}";
 
-                    return Results.Accepted(url, bookingInfo);
-                }
-                else
-                    throw new ValidationException("Не удалось создать объект бронирования так как объект события помечен как удаленный") { EntityId = id };
-            }
-            else
-                throw new NotFoundException("Не удалось создать объект бронирования так как объект события с указанным Id отсутствует") { EntityId = id };
+            return Results.Accepted(url, bookingInfo);
         }
     }
 }
