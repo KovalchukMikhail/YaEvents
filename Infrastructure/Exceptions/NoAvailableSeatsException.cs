2f9959be81c9f24b23019509adb0c837cb7751c9
KovalchukMikhail <kovalchuckmikhail@yandex.ru>
2026-04-17 14:45:10 +0300

    Добавлены новый тип исключений

---
+++ Infrastructure/Exceptions/NoAvailableSeatsException.cs
@@ -0,0 +1,10 @@
+﻿namespace YaEvents.Infrastructure.Exceptions
+{
+    public class NoAvailableSeatsException : Exception
+    {
+        public Guid? EntityId { get; set; }
+        public NoAvailableSeatsException() { }
+        public NoAvailableSeatsException(string message) : base(message) { }
+        public NoAvailableSeatsException(string? message, Exception? innerException) : base(message, innerException) { }
+    }
+}
