64a987284cfccae70f326897f41fc929aa0da60f
KovalchukMikhail <kovalchuckmikhail@yandex.ru>
2026-04-20 12:46:57 +0300

    Для хранения объектов применены конкурентные коллекции

--- Infrastructure/Repositories/BookingsRepository]/BookingsRepository.cs
+++ Infrastructure/Repositories/BookingsRepository]/BookingsRepository.cs
@@ -1,31 +1,39 @@
-﻿using YaEvents.Data.Models;
+﻿using System.Collections.Concurrent;
+using YaEvents.Data.Models;
 using YaEvents.Infrastructure.Repositories.Interfaces;
 
 namespace YaEvents.Infrastructure.Repositories.BookingsRepository
 {
     public class BookingsRepository : IRepository<Booking>
     {
-        protected static Dictionary<Guid, Booking> _bookings = new Dictionary<Guid, Booking>();
+        protected static ConcurrentDictionary<Guid, Booking> _bookings = new ConcurrentDictionary<Guid, Booking>();
         public virtual async Task<Booking> Add(Booking booking, CancellationToken token = default)
         {
-            _bookings[booking.Id] = booking;
+            _bookings.AddOrUpdate(booking.Id, booking, (id, b) => booking);
 
             return booking;
         }
 
         public virtual async Task Update(Booking booking, CancellationToken token = default)
         {
-            _bookings[booking.Id] = booking;
+            _bookings.AddOrUpdate(booking.Id, booking, (id, b) => booking);
         }
 
         public virtual async Task<bool> Delete(Guid id, CancellationToken token = default)
         {
-            return _bookings.Remove(id);
+            return _bookings.Remove(id, out Booking? booking);
         }
 
         public virtual async Task<Booking?> Get(Guid id, CancellationToken token = default)
         {
-            return _bookings.GetValueOrDefault(id);
+            if(_bookings.TryGetValue(id, out Booking? booking))
+            {
+                return booking;
+            }
+            else
+            {
+                return null;
+            }
         }
 
         public virtual async Task<IEnumerable<Booking>> GetAll(CancellationToken token = default)
