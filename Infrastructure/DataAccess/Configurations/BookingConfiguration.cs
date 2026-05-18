ecff19cd7050cbb34188660107e8954962e660f5
KovalchukMikhail <kovalchuckmikhail@yandex.ru>
2026-05-18 16:47:28 +0300

    Реализованы Fluent API для BookingConfiguration и EventConfiguration

---
+++ Infrastructure/DataAccess/Configurations/BookingConfiguration.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using YaEvents.Data.Models;
+
+namespace YaEvents.Infrastructure.DataAccess.Configurations
+{
+    public class BookingConfiguration : IEntityTypeConfiguration<Booking>
+    {
+        public void Configure(EntityTypeBuilder<Booking> builder)
+        {
+            builder.ToTable("bookings");
+
+            builder.Property(p => p.Id).ValueGeneratedNever();
+            builder.HasKey(p => p.Id);
+
+            builder.Property(p => p.Status)
+                    .IsRequired()
+                    .HasConversion<string>();
+
+            builder.Property(p => p.CreatedAt)
+                    .IsRequired();
+
+            builder.HasOne(b => b.Event)
+                    .WithMany(e => e.Bookings)
+                    .HasForeignKey(b => b.EventId);
+        }
+    }
+}
---
+++ Infrastructure/DataAccess/Configurations/EventConfiguration.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using YaEvents.Data.Models;
+
+namespace YaEvents.Infrastructure.DataAccess.Configurations
+{
+    public class EventConfiguration : IEntityTypeConfiguration<Event>
+    {
+        public void Configure(EntityTypeBuilder<Event> builder)
+        {
+            builder.ToTable("events");
+
+            builder.Property(p => p.Id).ValueGeneratedNever();
+            builder.HasKey(p => p.Id);
+
+            builder.Property(p => p.Title).IsRequired();
+
+            builder.Property(p => p.Description).IsRequired();
+        }
+    }
+}
