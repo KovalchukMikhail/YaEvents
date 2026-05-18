2404187343bf331e0b8d33085ced14635a9969fc
KovalchukMikhail <kovalchuckmikhail@yandex.ru>
2026-05-18 16:48:09 +0300

    Реализован DbContext

---
+++ Infrastructure/DataAccess/AppDbContext.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using YaEvents.Data.Models;
+
+namespace YaEvents.Infrastructure.DataAccess
+{
+    public class AppDbContext : DbContext
+    {
+        public DbSet<Event> Events => Set<Event>();
+        public DbSet<Booking> Bookings => Set<Booking>();
+
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.ApplyConfigurationsFromAssembly(typeof(AppDbContext).Assembly);
+        }
+
+    }
+}
