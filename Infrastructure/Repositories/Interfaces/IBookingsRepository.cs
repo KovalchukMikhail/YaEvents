d9c7cad0613b958bfce226975421c2fd132cb36c
KovalchukMikhail <kovalchuckmikhail@yandex.ru>
2026-05-21 13:51:33 +0300

    Созданы репозитории

---
+++ Infrastructure/Repositories/Interfaces/IBookingsRepository.cs
@@ -0,0 +1,13 @@
+﻿using YaEvents.Data.Models;
+
+namespace YaEvents.Infrastructure.Repositories.Interfaces
+{
+    public interface IBookingsRepository
+    {
+        public Task<Booking> Add(Booking booking, CancellationToken token = default);
+        public Task<Booking?> Get(Guid id, CancellationToken token = default);
+        public Task<Booking[]> GetPending(CancellationToken token = default);
+        public Task<bool> Confirm(Guid id, CancellationToken token = default);
+        public Task<bool> Reject(Guid id, CancellationToken token = default);
+    }
+}
