4035a33deb8cb9385ae33be63a79a75ac13d35aa
KovalchukMikhail <kovalchuckmikhail@yandex.ru>
2026-06-03 14:11:25 +0300

    В проект Domain перенесены доменные сущности, перечисления и доменные исключения

---
+++ Domain/Models/Booking.cs
@@ -0,0 +1,60 @@
+﻿using Domain.Enums;
+
+namespace Domain.Models
+{
+    public class Booking
+    {
+        public Guid Id { get; init; }
+        public Guid EventId { get; set; }
+        public BookingStatus Status { get; set; }
+        public DateTime CreatedAt { get; set; }
+        public DateTime? ProcessedAt { get; set; }
+        public Event? Event { get; set; }
+
+        private Booking() { }
+
+        public Booking(Guid id, Guid eventId, BookingStatus status, DateTime createdAt, DateTime? processedAt, Event? curEvent)
+        {
+            Id = id;
+            EventId = eventId;
+            Status = status;
+            CreatedAt = createdAt;
+            ProcessedAt = processedAt;
+            Event = curEvent;
+        }
+
+        public bool Confirm()
+        {
+            if (Status == BookingStatus.Confirmed)
+                return false;
+
+            Status = BookingStatus.Confirmed;
+            ProcessedAt = DateTime.Now.ToUniversalTime();
+            return true;
+        }
+        public bool Reject()
+        {
+            if (Status == BookingStatus.Rejected)
+                return false;
+
+            Status = BookingStatus.Rejected;
+            ProcessedAt = DateTime.Now.ToUniversalTime();
+            return true;
+        }
+        public override bool Equals(object? obj)
+        {
+            return obj is not null
+                && obj is Booking booking
+                && Id == booking.Id
+                && EventId == booking.EventId
+                && Status == booking.Status
+                && CreatedAt == booking.CreatedAt
+                && ProcessedAt == booking.ProcessedAt;
+        }
+
+        public override int GetHashCode()
+        {
+            return HashCode.Combine(Id, EventId, Status, CreatedAt, ProcessedAt);
+        }
+    }
+}
