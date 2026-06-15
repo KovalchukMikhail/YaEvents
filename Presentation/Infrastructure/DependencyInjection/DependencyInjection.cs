62554f89990cf3311e8bca31798bcdf853c7694b
KovalchukMikhail <kovalchuckmikhail@yandex.ru>
2026-06-15 19:40:23 +0300

    Добавлена логика для работы авторицации. Для swagger настроена автоизация

--- Presentation/Infrastructure/DependencyInjection/DependencyInjection.cs
+++ Presentation/Infrastructure/DependencyInjection/DependencyInjection.cs
@@ -3,10 +3,17 @@ using Application.Services.BackgroundServices;
 using Application.Services.BookingService;
 using Application.Services.EventService;
 using Application.Services.Interfaces;
+using Application.Services.SecurityServices;
+using Application.Services.UserServices;
 using Infrastructure.DataAccess;
 using Infrastructure.Repositories.BookingsRepository;
 using Infrastructure.Repositories.EventsRepository;
+using Infrastructure.Repositories.UsersRepository;
+using Infrastructure.Security;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.OpenApi;
+using System.Text.Json.Serialization;
 
 namespace Presentation.Infrastructure.DependencyInjection
 {
@@ -16,6 +23,7 @@ namespace Presentation.Infrastructure.DependencyInjection
         {
             services.AddScoped<IEventService, EventService>();
             services.AddScoped<IBookingService, BookingService>();
+            services.AddScoped<IUserService, UserService>();
 
             services.AddHostedService<BookingsBackgroundService>();
 
@@ -24,9 +32,31 @@ namespace Presentation.Infrastructure.DependencyInjection
 
         public static IServiceCollection AddPresentation(this IServiceCollection services)
         {
-            services.AddControllers();
+            services.AddControllers().AddJsonOptions(options =>
+            {
+                options.JsonSerializerOptions.Converters.Add(new JsonStringEnumConverter());
+            });
             services.AddEndpointsApiExplorer();
-            services.AddSwaggerGen();
+            services.AddSwaggerGen(options =>
+            {
+                options.SwaggerDoc("v1", new OpenApiInfo { Title = "YaEvents", Version = "v1" });
+
+                options.AddSecurityDefinition(JwtBearerDefaults.AuthenticationScheme, new OpenApiSecurityScheme
+                {
+                    Description = "Введите токен JWT в формате: Bearer {token}",
+                    Name = "Authorization",
+                    In = ParameterLocation.Header,
+                    Type = SecuritySchemeType.Http,
+                    Scheme = JwtBearerDefaults.AuthenticationScheme,
+                    BearerFormat = "JWT"
+                });
+
+                options.AddSecurityRequirement(document => new()
+                {
+                    [new OpenApiSecuritySchemeReference(JwtBearerDefaults.AuthenticationScheme, document)] = []
+                });
+
+            });
 
             return services;
         }
@@ -35,6 +65,8 @@ namespace Presentation.Infrastructure.DependencyInjection
         {
             services.AddScoped<IEventsRepository, EventsRepository>();
             services.AddScoped<IBookingsRepository, BookingsRepository>();
+            services.AddScoped<IUsersRepository, UsersRepository>();
+            services.AddScoped<ISecurityServise, SecurityServise>();
             services.AddDbContext<AppDbContext>(options => options.UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnection")));
 
             return services;
