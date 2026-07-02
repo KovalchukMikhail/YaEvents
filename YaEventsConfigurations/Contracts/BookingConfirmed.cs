d7d3fc7c0b136a0e79762071d20908aaecee69b9
KovalchukMikhail <kovalchuckmikhail@yandex.ru>
2026-07-02 20:28:59 +0300

    Формат сообщений и названия топика вынесено в отдельную библиотеку

---
+++ YaEventsConfigurations/Contracts/BookingConfirmed.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace YaEventsConfigurations.Contracts
+{
+    public class BookingConfirmed
+    {
+        public Guid MessageId { get; set; }
+        public Guid EventId { get; set; }
+        public Guid BookingId { get; set; }
+    }
+}
